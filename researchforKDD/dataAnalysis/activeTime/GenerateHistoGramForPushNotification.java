48658b0c2e001c337b911bca14dd9006551e5a83
beidou <beidou@zju.edu.cn>
2014-12-11 23:20:19 -0800

    push added

--- researchforKDD/dataAnalysis/activeTime/GenerateHistoGramForPushNotification.java
+++ researchforKDD/dataAnalysis/activeTime/GenerateHistoGramForPushNotification.java
@@ -2,12 +2,12 @@ package activeTime;
 
 import java.io.*;
 
-public class GenerateHistoGram {
+public class GenerateHistoGramForPushNotification {
 	
 	public void generatHisto(String inputpath,String outpath) throws Exception{
 		int[][] histo=new int [24][12];
 		int[][] histopush=new int [24][12];
-		long activities=0,push=0,opentimes=0;
+		long activities=0,pushhit=0,opentimes=0;
 				//iterate all files
 		File f=new File(inputpath);
 		File[] files=f.listFiles();
@@ -15,60 +15,70 @@ public class GenerateHistoGram {
 		for(int i=0;i<files.length;i++){
 			
 			try{
-				long old=0;
+				long old=0,timeinterval=0;
+				String oldtype="";
 				int[][] histoforone=new int [24][12];
 				BufferedReader br=new BufferedReader(new FileReader(files[i]));
 				String[] tnames=files[i].getAbsolutePath().split("/");
 				String wuid=tnames[tnames.length-1].replaceAll(".csv","");
 				String line="";
-				int otforone=0,acforone=0,pushforone=0;
+				int opentime;
+				int oldhour=0;
 				while((line=br.readLine())!=null){
 					
 					String[] t=line.split(",");
 					if(t==null||t.length<3){
 						continue;
 					}
-					System.out.println(t[0]);
 					String[] tmp=t[0].split(" ");
 					int hour=Integer.parseInt(tmp[1].split(":")[0]);
 					int minute=Integer.parseInt(tmp[1].split(":")[1]);
 
-					
+					long current=Long.parseLong(t[1]);
 					//System.out.println(line);
-					if(t[3].equals("push")){
-						push++;
-						pushforone++;
+					if(!t[3].equals("push")&&oldtype.equals("push")){
+						pushhit++;
+						timeinterval=(current-old)/60000;
 						histopush[hour][minute/5]++;
 						
+						if(oldhour<7){
+							save(timeinterval+"\r\n",outpath+"pushhitinterval_midnight.txt");
+						}
 						
-						continue;
+						if(oldhour>=7&&oldhour<=10){
+							save(timeinterval+"\r\n",outpath+"pushhitinterval_morning.txt");
+						}
+						
+						if(oldhour>10&&oldhour<14){
+							save(timeinterval+"\r\n",outpath+"pushhitinterval_noon.txt");
+						}
+						
+						if(oldhour>=14&&oldhour<17){
+							save(timeinterval+"\r\n",outpath+"pushhitinterval_afternoon.txt");
+						}
+						
+						if(oldhour>=17&&oldhour<=20){
+							save(timeinterval+"\r\n",outpath+"pushhitinterval_evening.txt");
+						}
+						
+						if(oldhour>=21){
+							save(timeinterval+"\r\n",outpath+"pushhitinterval_night.txt");
+						}
+
+						
+						
+						save(timeinterval+"\r\n",outpath+"pushhitinterval.txt");
 					}
-					activities++;
-					acforone++;
-					long current=Long.parseLong(t[1]);
-					if((current-old)<300000){
-						old=current;
-						continue;
-					}
+					
+					
+					
+					oldhour=hour;
 					old=current;
-					opentimes++;
-					otforone++;
+					oldtype=t[3];
 					
 					
-					histo[hour][minute/5]++;
-					histoforone[hour][minute/5]++;
 				}
-				save(""+acforone+"\r\n",outpath+"histo/"+wuid+".txt");
-				save(""+otforone+"\r\n",outpath+"histo/"+wuid+".txt");
-				save(""+pushforone+"\r\n",outpath+"histo/"+wuid+".txt");
-				for(int j=0;j<24;j++){
-					String line1="";
-					for(int k=0;k<12;k++){
-						line1+=j+":"+k*5+","+histoforone[j][k]+"\r\n";
-					}
-					
-					save(""+line1,outpath+"histo/"+wuid+".txt");
-				}
+				
 				
 				
 			}catch(Exception e){
@@ -77,17 +87,7 @@ public class GenerateHistoGram {
 		}
 		
 		
-		save(""+activities+"\r\n",outpath+"allhisto.txt");
-		save(""+opentimes+"\r\n",outpath+"allhisto.txt");
-		save(""+push+"\r\n",outpath+"allhisto.txt");
-		for(int j=0;j<24;j++){
-			String line1="";
-			for(int k=0;k<12;k++){
-				line1+=j+":"+k*5+","+histo[j][k]+"\r\n";
-			}
-			
-			save(""+line1,outpath+"allhisto.txt");
-		}
+		save(""+pushhit+"\r\n",outpath+"allhistopushhit.txt");
 		
 		for(int j=0;j<24;j++){
 			String line1="";
@@ -95,8 +95,10 @@ public class GenerateHistoGram {
 				line1+=j+":"+k*5+","+histopush[j][k]+"\r\n";
 			}
 			
-			save(""+line1,outpath+"allhistopush.txt");
+			save(""+line1,outpath+"allhistopushhit.txt");
 		}
+		
+		
 		
 		
 		
---
+++ researchforKDD/dataAnalysis/activeTime/RunHistoPush.java
@@ -0,0 +1,11 @@
+package activeTime;
+
+public class RunHistoPush {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		GenerateHistoGramForPushNotification g=new GenerateHistoGramForPushNotification();
+		g.generatHisto("/Users/beidou/research/kdd2014/log/log1211/outputlog/outputCSVPush/", "/Users/beidou/research/kdd2014/analysisinput/1211/");
+	}
+
+}
